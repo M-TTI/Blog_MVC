df157c3d7ee71c4c479a36399c5d220d6b5d6396
martin.bonetti <1martinbonetti@gmail.com>
2024-09-09 16:22:38 +0200

    added and coded UserDAO.php

--- Model/UserDAO.php
+++ Model/UserDAO.php
@@ -2,5 +2,46 @@
 
 class UserDAO
 {
+    private PDO $db;
 
+
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO('mysql:host=localhost;dbname=blog',
+                'itiz_mtti', 'password');
+        } catch(Exception $e) {
+            die('Error : '.$e->getMessage());
+        }
+    }
+
+    public function connectUser($username, $password) : bool
+    {
+        $sql = 'SELECT * FROM users WHERE username = ?';
+        $req = $this->db->prepare($sql);
+        $req->execute([$username]);
+
+        $user = $req->fetch();
+
+        if (!$user)
+        {
+            echo 'this user doesnt exist';
+        }
+        else
+        {
+            if($user['password'] == $password)
+            {
+                $sql = 'UPDATE users SET last_connection = NOW() WHERE username = ?';
+                $req = $this->db->prepare($sql);
+                $req->execute([$username]);
+
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['last_connection'] = $user['last_connection'];
+
+                return true;
+            }
+        }
+        return false;
+    }
 }
