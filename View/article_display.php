dd4c32df10fc9434202077b0f1284d4e18997e51
martin.bonetti <1martinbonetti@gmail.com>
2024-09-10 10:40:11 +0200

    added views for article display and article creation

--- View/article_display.php
+++ View/article_display.php
@@ -1,1 +1,10 @@
-<?php
+<small>Bonjour <?= $_SESSION['user_name'] ?></small>
+
+<h1>Tous les articles</h1>
+
+<?php foreach($articles as $a) { ?>
+    <h2><?= $a->title?> le <small><?= $a->publish_date ?></small></h2>
+    <p><?= $a->content ?></p>
+    <hr>
+<?php } ?>
+<a href="index.php?deco">Déconnexion</a>
--- View/create_article.php
+++ View/create_article.php
@@ -1,1 +1,7 @@
-<?php
+<form method="post" action="index.php">
+    <input type="text" name="title" placeholder="Titre">
+    <br>
+    <textarea name="content"></textarea>
+    <br>
+    <button name="btnCreate">Créer</button>
+</form>
