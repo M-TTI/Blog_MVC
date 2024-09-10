0180af2d0022909d94a7b03d6f5bac75b750906b
martin.bonetti <1martinbonetti@gmail.com>
2024-09-10 10:51:01 +0200

    added image_path support

--- Model/Article.php
+++ Model/Article.php
@@ -6,14 +6,16 @@ class Article
     private string $title;
     private string $publish_date;
     private string $content;
+    private string $image_path;
     private int $id_user;
 
-    public function __construct(int $id = 0, string $title = "default_title", string $publish_date = "2000-00-00", string $content = "default content", int $id_user = 0)
+    public function __construct(int $id = 0, string $title = "default_title", string $publish_date = "2000-00-00", string $content = "default content", string $image_path = "default.jpg", int $id_user = 0)
     {
         $this->id = $id;
         $this->title = $title;
         $this->publish_date = $publish_date;
         $this->content = $content;
+        $this->image_path = $image_path;
         $this->id_user = $id_user;
     }
 
@@ -29,6 +31,8 @@ class Article
                 return $this->publish_date;
             case 'content':
                 return $this->content;
+            case 'image_path':
+                return $this->image_path;
             case 'id_user':
                 return $this->id_user;
             default:
@@ -52,6 +56,9 @@ class Article
             case 'content':
                 $this->content = $value;
                 break;
+            case 'image_path':
+                $this->image_path = $value;
+                break;
             case 'id_user':
                 $this->id_user = $value;
                 break;
@@ -62,7 +69,7 @@ class Article
 
     public function __toString(): string
     {
-        return "Article id : ".$this->id."\ntitle : ".$this->title."\npublish date : ".$this->publish_date."\ncontent : ".$this->content."\n id_user : ".$this->id_user."\n";
+        return "Article id : ".$this->id."\ntitle : ".$this->title."\npublish date : ".$this->publish_date."\ncontent : ".$this->content."\n image_path : ".$this->image_path."\n id_user : ".$this->id_user."\n";
     }
 
 
--- Model/ArticleDAO.php
+++ Model/ArticleDAO.php
@@ -21,7 +21,7 @@ class ArticleDAO
         $req->execute([$id]);
         $articleData = $req->fetch();
 
-        return new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['id_user']);
+        return new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['image_path'], $articleData['id_user']);
     }
 
     public function getAll() : array
@@ -33,26 +33,26 @@ class ArticleDAO
         $articleArray = [];
         while ($articleData = $req->fetch())
         {
-            $articleArray[] += new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['id_user']);
+            $articleArray[] += new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['image_path'], $articleData['id_user']);
         }
         return $articleArray;
     }
 
     public function create(Article $article) : void
     {
-        $sql = "INSERT INTO article VALUES(:id, :title, :publish_date, :content, :id_user)";
+        $sql = "INSERT INTO article VALUES(:id, :title, :publish_date, :content, :image_path, :id_user)";
         $req = $this->db->prepare($sql);
         $req->execute(['id' => $article->id, 'title' => $article->title, 'publish_date' => $article->publish_date,
-            'content' => $article->content, 'id_user' => $article->id_user]);
+            'content' => $article->content, 'image_path' => $article->image_path, 'id_user' => $article->id_user]);
     }
 
     public function update(int $id, Article $article) : void
     {
-        $sql = "UPDATE article SET id = :new_id, title = :title, publish_date = :publish_date, content = :content, id_user = :user_id
+        $sql = "UPDATE article SET id = :new_id, title = :title, publish_date = :publish_date, content = :content, image_path = :image_path, id_user = :user_id
 WHERE id = :old_id";
         $req = $this->db->prepare($sql);
         $req->execute(['old_id' => $id, 'new_id' => $article->id, 'title' => $article->title, 'publish_date' => $article->publish_date,
-            'content' => $article->content, 'id_user' => $article->id_user]);
+            'content' => $article->content, 'image_path' => $article->image_path, 'id_user' => $article->id_user]);
     }
 
     public function delete(int $id) : void
