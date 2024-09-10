07f40a09e3fcfaa260f5cafc62de25b4521cf457
martin.bonetti <1martinbonetti@gmail.com>
2024-09-10 10:00:32 +0200

    added constructors, gets and CRUD methods

--- Model/ArticleDAO.php
+++ Model/ArticleDAO.php
@@ -2,5 +2,64 @@
 
 class ArticleDAO
 {
+    private PDO $db;
+
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO('mysql:host=localhost;dbname=blog',
+                'itiz_mtti', 'password');
+        } catch(Exception $e) {
+            die('Error : '.$e->getMessage());
+        }
+    }
+
+    public function getByID(int $id) : Article
+    {
+        $sql = "SELECT * FROM article WHERE id = ?";
+        $req = $this->db->prepare($sql);
+        $req->execute([$id]);
+        $articleData = $req->fetch();
+
+        return new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['id_user']);
+    }
+
+    public function getAll() : array
+    {
+        $sql = "SELECT * FROM article";
+        $req = $this->db->prepare($sql);
+        $req->execute();
+
+        $articleArray = [];
+        while ($articleData = $req->fetch())
+        {
+            $articleArray[] += new Article($articleData['id'], $articleData['title'], $articleData['publish_date'], $articleData['$content'], $articleData['id_user']);
+        }
+        return $articleArray;
+    }
+
+    public function create(Article $article) : void
+    {
+        $sql = "INSERT INTO article VALUES(:id, :title, :publish_date, :content, :id_user)";
+        $req = $this->db->prepare($sql);
+        $req->execute(['id' => $article->id, 'title' => $article->title, 'publish_date' => $article->publish_date,
+            'content' => $article->content, 'id_user' => $article->id_user]);
+    }
+
+    public function update(int $id, Article $article) : void
+    {
+        $sql = "UPDATE article SET id = :new_id, title = :title, publish_date = :publish_date, content = :content, id_user = :user_id
+WHERE id = :old_id";
+        $req = $this->db->prepare($sql);
+        $req->execute(['old_id' => $id, 'new_id' => $article->id, 'title' => $article->title, 'publish_date' => $article->publish_date,
+            'content' => $article->content, 'id_user' => $article->id_user]);
+    }
+
+    public function delete(int $id) : void
+    {
+        $sql = "DELETE FROM article WHERE id = ?";
+        $req = $this->db->prepare($sql);
+        $req->execute([$id]);
+    }
 
 }
--- Model/UserDAO.php
+++ Model/UserDAO.php
@@ -21,27 +21,76 @@ class UserDAO
         $req = $this->db->prepare($sql);
         $req->execute([$username]);
 
-        $user = $req->fetch();
+        $userData = $req->fetch();
 
-        if (!$user)
+        if (!$userData)
         {
             echo 'this user doesnt exist';
         }
         else
         {
-            if($user['password'] == $password)
+            if($userData['password'] == $password)
             {
                 $sql = 'UPDATE users SET last_connection = NOW() WHERE username = ?';
                 $req = $this->db->prepare($sql);
                 $req->execute([$username]);
 
-                $_SESSION['id'] = $user['id'];
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['last_connection'] = $user['last_connection'];
+                $_SESSION['id'] = $userData['id'];
+                $_SESSION['username'] = $userData['username'];
+                $_SESSION['last_connection'] = $userData['last_connection'];
+                $_SESSION['connected'] = true;
 
                 return true;
             }
         }
         return false;
+    }
+
+    public function getByID(int $id) : User
+    {
+        $sql = "SELECT * FROM users WHERE id = ?";
+        $req = $this->db->prepare($sql);
+        $req->execute([$id]);
+        $userData = $req->fetch();
+
+        return new User($userData['id'], $userData['username'], $userData['password'], $userData['last_connection']);
+    }
+
+    public function getAll() : array
+    {
+        $sql = "SELECT * FROM users";
+        $req = $this->db->prepare($sql);
+        $req->execute();
+
+        $userArray = [];
+        while ($userData = $req->fetch())
+        {
+            $userArray[] += new User($userData['id'], $userData['username'], $userData['password'], $userData['last_connection']);
+        }
+        return $userArray;
+    }
+
+    public function create(User $user) : void
+    {
+        $sql = "INSERT INTO users VALUES(:id, :username, :password, :last_connection)";
+        $req = $this->db->prepare($sql);
+        $req->execute(['id' => $user->id, 'username' => $user->username, 'password' => $user->password,
+            'last_connection' => $user->last_connection]);
+    }
+
+    public function update(int $id, User $user) : void
+    {
+        $sql = "UPDATE users SET id = :new_id, username = :username, password = :password, last_connection = :last_connection
+WHERE id = :old_id";
+        $req = $this->db->prepare($sql);
+        $req->execute(['old_id' => $id,'new_id' => $user->id, 'username' => $user->username, 'password' => $user->password,
+            'last_connection' => $user->last_connection]);
+    }
+
+    public function delete(int $id) : void
+    {
+        $sql = "DELETE FROM users WHERE id = ?";
+        $req = $this->db->prepare($sql);
+        $req->execute([$id]);
     }
 }
