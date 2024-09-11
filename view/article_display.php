307c474a792a94babff58c380d4d1c80bb8962a6
martin.bonetti <1martinbonetti@gmail.com>
2024-09-11 11:51:17 +0200

    added buttons for edit and delete articles

--- view/article_display.php
+++ view/article_display.php
@@ -5,6 +5,16 @@
 <?php foreach($articles as $a) { ?>
     <h2><?= $a->title?> le <small><?= $a->publish_date ?></small></h2>
     <p><?= $a->content ?></p>
+    <?php if ($a->id_user == $_SESSION['user_id']) { ?>
+            <form action="index.php" method="post">
+                <input type="hidden" value="<?= $a->id ?>" name="article_edit_id">
+                <input type="submit" value="edit" name="article_edit_button">
+            </form>
+            <form action="index.php" method="post">
+                <input type="hidden" value="<?= $a->id ?>" name="article_delete_id">
+                <input type="submit" value="delete" name="article_delete_button">
+            </form>
+        <?php }?>
     <hr>
 <?php } ?>
 <a href="index.php?deco">Déconnexion</a>
